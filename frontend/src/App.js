18b20e8f4f821c071da767e7d144afd709bca471
dipa180 <dipanwitap06@gmail.com>
2025-02-19 21:45:38 +0530

    Company SElection and Permission page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from './components/header';
 import InterviewPanel from './components/interviewPanel';
 
+import MockInterview from "./components/MockInterview";
+import Permissions from "./components/Permissions";
+
 function App() {
+    const [page, setPage] = useState("mock");
+
     return (
         <div className="App">
             <Header />
+
+            {page === "mock" && <MockInterview setPage={setPage} />}
+            {page === "permissions" && <Permissions setPage={setPage} />}
+
             <InterviewPanel />
         </div>
     );
