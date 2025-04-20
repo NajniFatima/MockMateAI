82777374bff69d780264748e88f995c32b4737a9
Najni Fatima <najnifatima01@gmail.com>
2025-04-20 22:31:32 +0530

    added chatgpt evaluation with .env.example file

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { OpenAI } from 'openai';
 
-const problemRoutes = require('./routes/dsaRoutes.js');
-const coreRoutes = require('./routes/coreRoutes.js');
+import { evaluationRoutes } from './routes/evaluationRoutes.js';
 
 const app = express();
 dotenv.config();
 
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/dsaRoutes', problemRoutes);
-app.use('/coreRoutes', coreRoutes);
+export const openai = new OpenAI({
+  apiKey: process.env.OPENROUTER_API_KEY,
+  baseURL: 'https://openrouter.ai/api/v1',
+});
+
+
+app.use('/api/evaluate', evaluationRoutes);
 
 const PORT = process.env.PORT || 5100;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running in PORT: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
--- frontend/src/components/interviewPanel/interviewPanel.js
+++ frontend/src/components/interviewPanel/interviewPanel.js
@@ -2,8 +2,20 @@ import React from 'react';
 import AIMessage from '../aiMessage/aiMessage';
 import VideoFeed from '../videoFeed/videoFeed';
 import './interviewPanel.css';
+import axios from "axios";
 
 function InterviewPanel() {
+    const handleEndInterview = async () => {
+        const response = await axios.post('http://localhost:5100/api/evaluate', {
+            question: "What is a closure in JavaScript?",
+            answer: "A closure is when you have a function inside another function.",
+          }, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+        console.log("ans :", response);
+    }
     return (
         <div className="interview-panel">
             <div className="ai-message">
@@ -13,7 +25,7 @@ function InterviewPanel() {
                 <VideoFeed />
             </div>
             <div className="end-interview">
-                <button className="end-btn">End Interview</button>
+                <button className="end-btn" onClick={handleEndInterview}>End Interview</button>
             </div>
         </div>
     );
